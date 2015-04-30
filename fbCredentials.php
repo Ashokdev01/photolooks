67176a1ea5937e5f8a88edc2db3a1da31b9371ae
divyesh-bhalala <dsbhalala99@gmail.com>
2015-04-30 17:58:33 +0530

    my change

--- fbCredentials.php
+++ fbCredentials.php
@@ -1,8 +1,8 @@
 <?php
 //Set Facebook App id and App Secret
 //Create our Application instance (replace this with your appId and secret).
-$appId = $AppId = '485247754943027';                    //'1560505420851840';                          //485247754943027
-$appSecret = $AppSecret = 'e1abda0f213cc9dbe60c236c8be684ab';  //'bdddcd25a93bb9ce8e5fcb564f9f9280';      //'e1abda0f213cc9dbe60c236c8be684ab';
+$appId = $AppId = '932142060171589';                    //'1560505420851840';                          //485247754943027
+$appSecret = $AppSecret = '2629623e5e40c416606e5ab453434221';  //'bdddcd25a93bb9ce8e5fcb564f9f9280';      //'e1abda0f213cc9dbe60c236c8be684ab';
 
 ?>
 
